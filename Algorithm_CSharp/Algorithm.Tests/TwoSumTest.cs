4efcfea55515254492569c9a266929628c9dae9b
Hank.Huang <hank.huang@xuenn.com>
2026-03-24 17:33:03 +0800

    dev&test TwoSum

---
+++ Algorithm_CSharp/Algorithm.Tests/TwoSumTest.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithm.Tests;
+
+[TestFixture]
+[TestOf(typeof(TwoSum))]
+public class TwoSumTest
+{
+    [TestCase(new int[] { 2, 7, 11, 15 }, 9, new [] {0,1})]
+    [TestCase(new int[] { 3,2,4 }, 6, new [] {1, 2})]
+    [TestCase(new int[] { 3,3 }, 6, new [] {0, 1})]
+    public void TwoSum_O2n(int[] nums, int target, int[] expected)
+    {
+        var sut = new TwoSum();
+
+        var result = sut.Execute(nums, target);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
