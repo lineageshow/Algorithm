42257209b2e2db082066a86f64752c4c6fb4f4be
Hank Huang <lineageshow@gmail.com>
2026-04-15 16:59:47 +0800

    add unit test for InorderTraversalWithStack Tests

--- Algorithm_CSharp/Algorithm.Tests/Graph/BinaryTreeInorderTraversalTest.cs
+++ Algorithm_CSharp/Algorithm.Tests/Graph/BinaryTreeInorderTraversalTest.cs
@@ -20,6 +20,17 @@ public class BinaryTreeInorderTraversalTest
         
         result.Should().BeEquivalentTo(new int[] { 1, 3, 2 }, options => options.WithStrictOrdering());
     }
+    [Test]
+    public void InorderTraversalWithStack_Test_Example1()
+    {
+        // root = [1,null,2,3]
+        var root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+        var sut = new BinaryTreeInorderTraversal();
+        
+        var result = sut.InorderTraversalWithStack(root);
+        
+        result.Should().BeEquivalentTo(new int[] { 1, 3, 2 }, options => options.WithStrictOrdering());
+    }
 
     [Test]
     public void Test_Example2()
@@ -32,16 +43,36 @@ public class BinaryTreeInorderTraversalTest
         
         result.Should().BeEquivalentTo(new int[] { }, options => options.WithStrictOrdering());
     }
+    [Test]
+    public void InorderTraversalWithStack_Test_Example2()
+    {
+        // root = []
+        TreeNode root = null;
+        var sut = new BinaryTreeInorderTraversal();
+        
+        var result = sut.InorderTraversalWithStack(root);
+        
+        result.Should().BeEquivalentTo(new int[] { }, options => options.WithStrictOrdering());
+    }
 
     [Test]
     public void Test_Example3()
     {
-        // root = [1]
         var root = new TreeNode(1);
         var sut = new BinaryTreeInorderTraversal();
         
         var result = sut.InorderTraversalWithRecursive(root);
         
         result.Should().BeEquivalentTo(new int[] { 1 }, options => options.WithStrictOrdering());
+    }
+
+    [Test]
+    public void InorderTraversalWithStack_Test_Example3()
+    {
+        TreeNode root = new TreeNode(1);
+        var sut = new  BinaryTreeInorderTraversal();
+        var result = sut.InorderTraversalWithStack(root);
+        result.Should().BeEquivalentTo(new int[] { 1 }, options => options.WithStrictOrdering());
+
     }
 }
